445215dd39b34b007e50c07b1fec8970dfd24d4a
Gavin Reid <gavingq94@gmail.com>
2025-05-30 21:11:28 -0400

    pj15

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const clothingItems = require("./clothingitems");
+const clothingItems = require("./clothingItems");
 const userRouter = require("./users");
 const { STATUS_CODES } = require("../utils/errors");
 const { login, createUser } = require("../controllers/users");
